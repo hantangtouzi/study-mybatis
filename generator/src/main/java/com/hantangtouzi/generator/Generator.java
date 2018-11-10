e1bd7b104e530c9dfee6cba663da6f6d7f9004eb
hantangtouzi <hantangtouzi@foxmail.com>
2018-11-10 20:29:01 +0800

    添加模块：generator

---
+++ generator/src/main/java/com/hantangtouzi/generator/Generator.java
@@ -0,0 +1,45 @@
+package com.hantangtouzi.generator;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author WilliamChang.
+ * Created on 2018/11/10 20:02
+ */
+
+public class Generator {
+    public void generator() throws Exception {
+
+        String file="D:\\Tools\\idea\\projects\\study-mybatis\\generator\\src\\main\\resources\\generatorConfig.xml";
+
+        List<String> warnings = new ArrayList<>();
+        boolean overwrite = true;
+        //指定 逆向工程配置文件
+        File configFile = new File(file);
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            Generator generator = new Generator();
+            generator.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
